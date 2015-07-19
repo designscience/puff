e508eca39b893f68722a56de82c1f8b1fdc6cf36
Stu D'Alessandro <studa@design-sci.com>
2015-07-18 18:58:32 -0700

    Added code to recover from unexpected disconnect and revert to waiting for a connection

--- Puff.py
+++ Puff.py
@@ -17,6 +17,7 @@ def main(argv):
     bufsize = 1024
     addr = (host, port)
     local_addr = gethostbyname(gethostname())
+    running = True # keep running until this is set false
 
     # process command line arguments
     try:
@@ -39,19 +40,29 @@ def main(argv):
     # Open socket
     ss = socket(AF_INET, SOCK_STREAM)
     ss.bind(addr)
-    ss.listen(1)
-    while True:
+    while running:
+        ss.listen(1)
         print "Listening on host {0}, port {1}".format(local_addr, port)
         cs, addr = ss.accept() # blocking
         print 'Connected from client at addr'
         while True:
-            mssg = cs.recv(bufsize)
+            try:
+                mssg = cs.recv(bufsize)
+            except error:
+                print "Lost connection to host process. Waiting for new connection..."
+                break
             if not mssg:
+                print "Null data received. Stopping program."
+                running = False
                 break
             else:
                 print "Message: {0}".format(mssg)
+
+            # TODO: testing only. Replace with message processing
             cs.send(mssg)
+
         cs.close()
+
     ss.close()
     return 0
 
