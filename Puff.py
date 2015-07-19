7791b4653a73e1f9c6ab8bed9371d4f65fb98d7b
Stu D'Alessandro <studa@design-sci.com>
2015-07-19 00:26:10 -0700

    Corrected message nibbling

--- Puff.py
+++ Puff.py
@@ -29,9 +29,8 @@ def parse_and_execute(data, bank):
                 command = message.group(1)
                 version = message.group(2)
                 params = message.group(3)
-                data = data[:message.end(0)]
+                data = data[message.end(0):]
                 print "Message end: {0} Command: {1}, params: {2}, data now {3}".format(message.end(0), command, params, data)
-                data = ""
             else:
                 data = ""
                 break
